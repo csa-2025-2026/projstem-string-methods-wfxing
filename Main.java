94565825609e7c4e6ec85cd0b62f633d57c0c145
Kyle Vu <ktvu@hbuhsd.edu>
2025-09-28 20:37:46 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,4 +4,28 @@ public class Main
   {
     // write your code here
   }
+
+  /** Precondition: s1 and s2 are not null
+  * Precondition: 0 <= n <= min(s1.length(), s2.length()
+  */
+  public static void lastFirstN(String s1, String s2, int n)
+  {
+  
+  }
+
+  /** Precondition: s1 and s2 are not null
+  */
+  public static void stringManip(String s1, String s2)
+  {
+  
+  }
+
+  /** Precondition: s1 and s2 are not null
+  * Precondition: s2 is a substring of s1 and can be found at least one time in s1
+  */
+  public static String removeStr(String s1, String s2)
+  {
+    String output = null;
+    return output;
+  }
 }
